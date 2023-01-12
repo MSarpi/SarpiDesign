2e6b05e5237216a6d16a2c9e9253a69a973d1d16
msarpi1 <msarpi@mastransporte.cl>
2023-01-12 02:03:11 -0300

    arreglo varios

--- js/scripts_ms.js
+++ js/scripts_ms.js
@@ -21,28 +21,37 @@ function formulario(){
 	}
 
 	if (val == 0) {
-		alert("funciono");
-		$('#titulo_modal_header').html('<h5>Mensaje Enviado</h5>');
-		$('#body_modal_header').html('<p>Me pondre en contacto contigo de 24 a 48h!, muchas gracias.</p>');
-		$('#exampleModal').modal('show');
-		// $.ajax({
-		// 	type: "POST",
-		// 	url: "/enviar.php",
-		// 	data: formData,
-		// 	cache: false,
-		// 	processData: false,
-		// 	contentType: false,
-		// 	success: function (res) {
-		// 		alert("funciono");
-		// 	},
-		// 	error:function (erro) {
-		// 		alert(JSON.stringify(erro));
-		// 	}
-		// });
+		var gmail = document.getElementById('email').value;
+		var nom = document.getElementById('name').value;
+		var text = document.getElementById('textarea').value;
+
+		$.ajax({
+			type: "POST",
+			url: "https://formsubmit.io/send/670d39ef-8b75-43db-9fe1-e39e7a75f9d4",
+			dataType: 'json',
+			accepts: 'application/json',
+			data: {
+				Nombre: nom,
+				Gmail: gmail,
+				Informacion: text
+			},
+			success: function (res) {
+				
+			},
+			error:function (erro) {
+				$('#titulo_modal').html('<h5>Mensaje Enviado con exito</h5>');
+				$('#body_modal').html('<p>Me pondre en contacto contigo en 24 a 48h!, muchas gracias.</p>');
+				$('#modal_exito').modal('show');
+			}
+		});
 	}else{
 		alert(JSON.stringify(error));
 		$('#titulo_modal_header').html('<h5>Falta Rellenar Campos</h5>');
 		$('#body_modal_header').html('<p>'+error+'</p>');
 		$('#exampleModal').modal('show');
 	}
+}
+
+function reload(){
+	location.reload(true);
 }
