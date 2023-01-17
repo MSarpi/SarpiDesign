df3a44c874b903937287dab7470de79874064080
msarpi1 <msarpi@mastransporte.cl>
2023-01-16 23:55:43 -0300

    arreglo varios

--- js/scripts_ms.js
+++ js/scripts_ms.js
@@ -68,21 +68,3 @@ function configureLoadingScreen(screen){
 function reload(){
 	location.reload(true);
 }
-
-$(document).ready(function(){
-	$('#up').hide();
-	$(window).scroll(function(){
-		if ($(this).scrollTop()>100) {
-			$('.up').fadeIn('1000');
-		}else{
-			$('.up').fadeOut('1000');
-		}
-	});
-
-	$('.up').click(function () { 
-		$('body, html').animate({
-			scrollTop: 0
-		})
-		
-	});
-});
